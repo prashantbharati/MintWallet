4c11327e39e04ee3aa8c29934a6e8948f744f6a9
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-06-06 10:04:34 +0530

    home test routes built

--- client/src/pages/Test.js
+++ client/src/pages/Test.js
@@ -1,11 +1,12 @@
 import React from "react";
 import DefaultLayout from "../components/DefaultLayout";
-function Home() {
+
+function Test() {
   return (
     <DefaultLayout>
-      <h1>This is home page</h1>
+      <h1>This is test page</h1>
     </DefaultLayout>
   );
 }
 
-export default Home;
+export default Test;
