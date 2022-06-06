230150871d7a393ddfc09ca2e72bf2b52410a88e
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-06-06 08:33:10 +0530

    antd default css added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import "antd/dist/antd.css";
+import { Button } from "antd";
 function App() {
   return (
     <div className="App">
       <h1>MintWallet</h1>
+      <Button type="primary">Primary button</Button>
     </div>
   );
 }
