897d74b9f0c233ff648a9aab763dec9f1a168d63
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-06-07 09:26:27 +0530

    lottie animation added+ cssified

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -2,13 +2,24 @@ import { Form } from "antd";
 import Input from "antd/lib/input/Input";
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import "../resources/authentication.css";
 function Register() {
   return (
     <div className="register">
-      <div className="row">
-        <div className="col-md-5"></div>
-
+      <div className="row justify-content-center align-items-center">
         <div className="col-md-5">
+          <div className="lottie">
+            <lottie-player
+              src="https://assets3.lottiefiles.com/packages/lf20_06a6pf9i.json"
+              background="transparent"
+              speed="1"
+              loop
+              autoplay
+            ></lottie-player>
+          </div>
+        </div>
+
+        <div className="col-md-4">
           <Form layout="vertical">
             <Form.Item label="Name" name="name">
               <Input />
@@ -22,7 +33,7 @@ function Register() {
 
             <div className="d-flex justify-content-between align-items-center">
               <Link to="/login">Already Registered , Click Here To Login</Link>
-              <button className="secondary" type="submit">
+              <button className="primary" type="submit">
                 REGISTER
               </button>
             </div>
