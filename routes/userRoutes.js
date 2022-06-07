6693f1f6cf2e30af6bdc03dcf1522d3278f02249
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-06-07 10:12:26 +0530

    user post route done

---
+++ routes/userRoutes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+router.post("/login", async function (req, res) {
+  try {
+    const result = await User.findOne({
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    if (result) {
+      res.send(result);
+    } else {
+      res.status(500).json("Error");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
