3a05de2e9377d475aa9f1dd4ac67253b99e0d9dd
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-06-09 22:52:37 +0530

    add new popup done

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DefaultLayout from "../components/DefaultLayout";
 import "../resources/transactions.css";
-
+import Modal from "antd/lib/modal/Modal";
 function Home() {
+  const [showAddEditTransactionModal, setShowAddEditTransactionModal] =
+    useState(false);
   return (
     <DefaultLayout>
       <div className="filter d-flex justify-content-between align-items-center">
         <div className="d-flex"></div>
 
         <div className="d-flex">
-          <button className="primary">ADD NEW</button>
+          <button
+            className="primary"
+            onClick={() => setShowAddEditTransactionModal(true)}
+          >
+            ADD NEW
+          </button>
         </div>
       </div>
 
       <div className="table-analtics"></div>
+
+      <Modal
+        title="Add Transaction"
+        visible={showAddEditTransactionModal}
+        onCancel={() => setShowAddEditTransactionModal(false)}
+      ></Modal>
     </DefaultLayout>
   );
 }
