272ee48da8b9814eb7e067016d2c856d53a95bd8
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-06-10 20:46:02 +0530

    get-transaction error removed

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,36 @@
 import React, { useEffect, useState } from "react";
 import DefaultLayout from "../components/DefaultLayout";
-
+import axios from "axios";
 import "../resources/transactions.css";
 import AddEditTransaction from "../components/AddEditTransaction";
 function Home() {
   const [showAddEditTransactionModal, setShowAddEditTransactionModal] =
     useState(false);
+  const [loading, setLoading] = useState(false);
+  const getTransactions = async () => {
+    try {
+      const user = JSON.parse(localStorage.getItem("sheymoney-udemy-user"));
+
+      setLoading(true);
+      const response = await axios.post(
+        "/api/transactions/get-all-transactions",
+        { userid: user._id }
+      );
+
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      message.error("Something wennt wrong");
+    }
+  };
+
+  useEffect(() => {
+    getTransactions();
+  }, []);
+
   return (
     <DefaultLayout>
+      {loading && <Spinner />}
       <div className="filter d-flex justify-content-between align-items-center">
         <div className="d-flex"></div>
 
