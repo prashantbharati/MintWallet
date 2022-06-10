a0f0fef94add4a555c9736f3aa8143b79c9109ff
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-06-10 20:09:30 +0530

    transcation error removed

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
 const transactionSchema = new mongoose.Schema({
-  userid: { type: String, required: true },
   amount: { type: Number, required: true },
   type: { type: String, required: true },
   category: { type: String, required: true },
--- routes/transactionsRoute.js
+++ routes/transactionsRoute.js
@@ -5,6 +5,7 @@ const moment = require("moment");
 const { response } = require("express");
 router.post("/add-transaction", async function (req, res) {
   try {
+    console.log(req.body);
     const newtransaction = new Transaction(req.body);
     await newtransaction.save();
     res.send("Transaction Added Successfully");
