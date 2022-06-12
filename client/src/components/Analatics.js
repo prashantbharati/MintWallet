524a165bfbc505e4aab143df1f11d3376d32ebf7
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-06-12 19:05:05 +0530

    transaction count done with calculations

--- client/src/components/Analatics.js
+++ client/src/components/Analatics.js
@@ -8,8 +8,27 @@ function Analatics({ transactions }) {
   const totalExpenceTransactions = transactions.filter(
     (transaction) => transaction.type === "expence"
   );
+  const totalIncomeTransactionsPercentage =
+    (totalIncomeTransactions.length / totalTransactions) * 100;
+  const totalExpenceTransactionsPercentage =
+    (totalExpenceTransactions.length / totalTransactions) * 100;
 
-  return <div>Analatics</div>;
+  return (
+    <div>
+      <div className="row">
+        <div className="col-md-4">
+          <div className="transactions-count">
+            <h4>Total Transactions : {totalTransactions}</h4>
+            <hr />
+            <h5>Income : {totalIncomeTransactions.length}</h5>
+            <h5>Expence : {totalExpenceTransactions.length}</h5>
+
+            <div className="progress-bars"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Analatics;
