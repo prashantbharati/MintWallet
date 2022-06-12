4d8b0ac2e72ea67965df93a25eb3181a2d3dabd4
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-06-12 19:17:27 +0530

    progress bars added

--- client/src/components/Analatics.js
+++ client/src/components/Analatics.js
@@ -1,5 +1,6 @@
+import { Progress } from "antd";
 import React from "react";
-
+import "../resources/analatics.css";
 function Analatics({ transactions }) {
   const totalTransactions = transactions.length;
   const totalIncomeTransactions = transactions.filter(
@@ -14,16 +15,28 @@ function Analatics({ transactions }) {
     (totalExpenceTransactions.length / totalTransactions) * 100;
 
   return (
-    <div>
+    <div className="analytics">
       <div className="row">
-        <div className="col-md-4">
+        <div className="col-md-4 mt-3">
           <div className="transactions-count">
             <h4>Total Transactions : {totalTransactions}</h4>
             <hr />
             <h5>Income : {totalIncomeTransactions.length}</h5>
             <h5>Expence : {totalExpenceTransactions.length}</h5>
 
-            <div className="progress-bars"></div>
+            <div className="progress-bars">
+              <Progress
+                className="mx-5"
+                strokeColor="#5DD64F"
+                type="circle"
+                percent={totalIncomeTransactionsPercentage.toFixed(0)}
+              />
+              <Progress
+                strokeColor="#E5572F"
+                type="circle"
+                percent={totalExpenceTransactionsPercentage.toFixed(0)}
+              />
+            </div>
           </div>
         </div>
       </div>
