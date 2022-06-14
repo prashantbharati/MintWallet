750ec40c8c85cf57f54b14fd3dc075504aef7848
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-06-14 15:52:15 +0530

    make it better

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+dotenv.config();
 const app = express();
 app.use(express.json());
 const path = require("path");
@@ -9,7 +10,6 @@ const transactionsRoute = require("./routes/transactionsRoute");
 app.use("/api/users/", userRoute);
 app.use("/api/transactions/", transactionsRoute);
 app.get("/", (req, res) => res.send("Hello world"));
-dotenv.config();
 
 // Setting up the database with the help of Mongoose and env variables
 
