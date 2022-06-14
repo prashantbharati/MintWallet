09427209efc0722a6ec072609e26a8f7835f2a3f
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-06-14 15:59:35 +0530

    make it better

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-dotenv.config();
+dotenv.config({ path: "./.env" });
 const app = express();
 app.use(express.json());
 const path = require("path");
@@ -13,15 +13,15 @@ app.get("/", (req, res) => res.send("Hello world"));
 
 // Setting up the database with the help of Mongoose and env variables
 
-// if (process.env.NODE_ENV === "production") {
-//   // Frontend
-//   app.use("/", express.static("client/build"));
+if (process.env.NODE_ENV === "production") {
+  // Frontend
+  app.use("/", express.static("client/build"));
 
-//   // Backend
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client/build/index.html"));
-//   });
-// }
+  // Backend
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+  });
+}
 
 mongoose
   .connect(process.env.CONNECTION_URL, {
