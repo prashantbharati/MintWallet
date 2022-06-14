9de78a65e241be1bb36aaad76f2bd0480f7c3b7a
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-06-14 16:08:00 +0530

    make it better

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-dotenv.config({ path: ".env" });
+dotenv.config();
 const app = express();
 app.use(express.json());
 const path = require("path");
